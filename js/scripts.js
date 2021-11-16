26d3679b0dc8d156c8341556231d09ef09b20c41
adamgutt <93285321+adamgutt@users.noreply.github.com>
2021-11-15 19:00:03 -0500

    Created 3 pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-let pokemonList = [{},{},{}];
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ['grass']['poison']
+  },
+  {
+    name: "Venusaur",
+    height: 2,
+    types: ['grass']['poison']
+  },
+  {
+    name: "Beedrill",
+    height: 1,
+    types: ['bug']['poison']
+  }
+];
