1b0d016a3a32e68b17ccfa602323b7c228397faa
adamgutt <93285321+adamgutt@users.noreply.github.com>
2021-11-15 19:07:33 -0500

    Fixed pokemon objects to work

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ let pokemonList = [
   {
     name: "Bulbasaur",
     height: 0.7,
-    types: ['grass']['poison']
+    types: ['grass','poison']
   },
   {
     name: "Venusaur",
     height: 2,
-    types: ['grass']['poison']
+    types: ['grass','poison']
   },
   {
     name: "Beedrill",
     height: 1,
-    types: ['bug']['poison']
+    types: ['bug','poison']
   }
 ];
