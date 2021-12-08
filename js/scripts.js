0a2165b025e2322bcf11005367cac1b94339b618
adamgutt <93285321+adamgutt@users.noreply.github.com>
2021-12-08 15:49:55 -0500

    Changed from for loop to forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,20 @@ let pokemonList = [
   }
 ];
 
-for (let i = 0; i < 3; i++){
+// for (let i = 0; i < 3; i++){
+//   document.write('<li>')
+//   document.write(pokemonList[i].name + " (height: "+ pokemonList[i].height + ") ");
+//   if (pokemonList[i].height > 1.5){
+//     document.write(" - Wow, that's big!");
+//   }
+//   document.write("</li>")
+// }
+
+pokemonList.forEach(function(user){
   document.write('<li>')
-  document.write(pokemonList[i].name + " (height: "+ pokemonList[i].height + ") ");
+  document.write(user.name + " (height: "+ user.height + ") ");
+});
   if (pokemonList[i].height > 1.5){
     document.write(" - Wow, that's big!");
   }
   document.write("</li>")
-}
