9648fdbb3bddfa159d02761aef1a9932ab6731aa
adamgutt <93285321+adamgutt@users.noreply.github.com>
2021-12-08 18:44:19 -0500

    updating object using IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,47 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ['grass','poison']
-  },
-  {
-    name: "Venusaur",
-    height: 2,
-    types: ['grass','poison']
-  },
-  {
-    name: "Beedrill",
-    height: 1,
-    types: ['bug','poison']
-  }
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ['grass','poison']
+    },
+    {
+      name: "Venusaur",
+      height: 2,
+      types: ['grass','poison']
+    },
+    {
+      name: "Beedrill",
+      height: 1,
+      types: ['bug','poison']
+    }
+  ];
 
-pokemonList.forEach(function(user){
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+    if (typeOf() !== "object"){
+      return true;
+    }
+  }
+
+  function getAll(){
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+
+pokemonRepository.getAll().forEach(function(user){
   document.write('<li>')
   document.write(user.name + " (height: "+ user.height + ") ");
-});
-  if (pokemonList[i].height > 1.5){
+  if (user.height > 1.5){
     document.write(" - Wow, that's big!");
   }
-  document.write("</li>")
+});
+
+document.write(pokemonRepository.getAll());
